a65b78e0c72db103e2ce63df888f563f9d1bea4c
christa-baccas <christa.baccas@gmail.com>
2021-10-16 10:22:45 -0500

    added engineer class and updated the unit tests

---
+++ _tests_/Engineer.test.js
@@ -0,0 +1,24 @@
+const Engineer = require('../lib/Engineer')
+
+describe("Employee class", () => {
+    it("should show the input types of each argument passed from the parent including the new github ", () => {
+      const engineer = new Engineer('Christa', 44, 'christa.baccas@gmail.com', 'christa-baccas');
+  
+      expect(engineer.name).toEqual(expect.any(String));
+      expect(engineer.id).toEqual(expect.any(Number));
+      expect(engineer.email).toEqual(expect.any(String));
+      expect(engineer.github).toEqual(expect.any(String));
+    });
+  
+    it("should get the engineer role and return `engineer` ", () => {
+        const engineer = new Engineer('Christa', 44, 'christa.baccas@gmail.com', 'christa-baccas');
+        const result= 'Engineer'
+        expect(engineer.getRole()).toEqual(result);
+    });
+
+    it("should get the engineers github and return their github username ", () => {
+        const engineer = new Engineer('Christa', 44, 'christa.baccas@gmail.com', 'christa-baccas');
+        const result= `https://github.com/christa-baccas`
+        expect(engineer.getGithub()).toEqual(result);
+      });
+  });
