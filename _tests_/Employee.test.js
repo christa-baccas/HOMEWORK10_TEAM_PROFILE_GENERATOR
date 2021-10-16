3512d316663c04dc341b83de044ba2401b0aece8
christa-baccas <christa.baccas@gmail.com>
2021-10-16 08:35:33 -0500

    updated unit test for the employee js file

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,3 +1,38 @@
 // import Employee constructor
 const Employee = require('../lib/Employee')
 
+describe("Employee class", () => {
+  it("should show the input types of each argument passed", () => {
+    //results upon entering
+    const employee = new Employee('Christa', 44, 'christa.baccas@gmail.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+  });
+
+  it("should get the name of the employee", () => {
+    const employee = new Employee('Christa', 44, 'christa.baccas@gmail.com');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+  });
+
+  it("should get the id of the employee", () => {
+    const employee = new Employee('Christa', 44, 'christa.baccas@gmail.com');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+  });
+
+  it("should get the email of the employee", () => {
+    const employee = new Employee('Christa', 44, 'christa.baccas@gmail.com');
+
+    expect(employee.getEmail()).toEqual(expect.any(String));
+  });
+
+  it("should get the role of the employee", () =>{
+    const employee = new Employee('Christa', 44, 'christa.baccas@gmail.com')
+
+    expect(employee.getRole()).toEqual(expect.any(String))
+  })
+
+});
